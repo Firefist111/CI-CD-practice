a28ce567fe4c27615374af2e2f5ea0462a7851c1
Aniket <aniketnaik2004@gmailcom>
2026-09-09 13:56:43 +0530

    Changes from f1

--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/ecommerce';
 
+const name = "Helzcaicuscisnsicnscisn";
+
 app.use(cors());
 app.use(express.json());
 
